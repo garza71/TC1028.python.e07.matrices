8aa474a3cbefad079a50fa22c8cbdda812e71bc3
Lina Patricia Garza Gomez <57541431+garza71@users.noreply.github.com>
2021-09-25 18:56:17 -0500

    Update exercise.py

--- assignments/12CreaMatrizDeNumConsec/src/exercise.py
+++ assignments/12CreaMatrizDeNumConsec/src/exercise.py
@@ -1,30 +1,6 @@
 def main():
     #escribe tu código abajo de esta línea
 
-    #Cantidad de renglones
-    r = int(input())
-    c = int(input())
-
-    if r >= 2 and c >=2:
-        #Matriz vacía
-        matriz = []
-        #Crear la matriz 
-        num = 1
-        for x in range(r):
-            #Renglon vacío
-            renglon = []
-            #Crear un renglon
-            for x in range(c):
-                renglon.append(num)
-                num = num + 1
-            matriz.append(renglon)
-        #Imprime la matriz
-        print(matriz)
-    else:
-        print("Error")
-
-
-
 
 if __name__=='__main__':
     main()
